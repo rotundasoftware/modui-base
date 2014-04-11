583757981cbe3a3b9287f84227c5e0b09edd1300
David Beck <david@rotundasoftware.com>
2014-04-11 10:50:36 -0700

    second commit

---
+++ modui-base.js
@@ -0,0 +1,33 @@
+var Super = require( 'backbone' ).View;
+
+var viewOptions = require( 'backbone-view-options' );
+var courier = require( 'backbone-courier' );
+var handle = require( 'backbone-handle' );
+var subviews = require( 'backbone-subviews' );
+
+module.exports = Super.extend( {
+	constructor : function( options ) {
+		viewOptions.add( this );
+		this.setOptions( options );
+		
+		// needs to be done before super is called, since super calls view#initialize,
+		// and view#initialize might spawn events
+		courier.add( this );
+		handle.add( this ); // needs to happen after Courier.add, so handles get replaced in spawnMessages
+
+		var returnValue = Super.prototype.constructor.apply( this, arguments );
+
+		// must be done after super is called
+		subviews.add( this );
+
+		return returnValue;
+	},
+
+	render : function() {
+		this.$el.html( this.template.render( this._getTemplateData() ) );
+	},
+
+	_getTemplateData : function() {
+		return this.getOptions();
+	}
+} );
